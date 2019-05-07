a53374b97f39f95e3f380967bffd8ae7902c85af
Marek Zawadzki <marek.zawadzki@nordcloud.com>
2019-05-07 14:14:49 +0200

    Fix outputs

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,14 @@
 output "r53_private_zones" {
   description = "List of names of private Route53 zones created"
-
-  value = "${aws_route53_zone.this.*.name}"
+  value       = "${aws_route53_zone.this.*.name}"
 }
 
 output "r53_records" {
   description = "List of names of Route53 records created"
+  value       = "${aws_route53_record.this.*.name}"
+}
 
-  value = "${aws_route53_record.this.*.name}"
+output "vpce_id" {
+  description = "The ID of the VPC endpoint"
+  value       = "${aws_vpc_endpoint.vpce.id}"
 }
