39d237920d79177b2db9005cee469cfa85b6c34d
Marek Zawadzki <marek.zawadzki@nordcloud.com>
2019-05-09 14:30:08 +0200

    Update module source for config repo template

--- bootstrap/configuration_repo_template/main.tf
+++ bootstrap/configuration_repo_template/main.tf
@@ -1,5 +1,5 @@
 module "vpc-endpoint" {
-  source = "github.com/kentrikos/terraform-aws-shared-proxy-vpc-endpoint.git?ref=zero-config"
+  source = "github.com/kentrikos/terraform-aws-shared-proxy-vpc-endpoint.git"
 
   vpc_endpoint_service_name = "${var.vpc_endpoint_service_name}"
   vpc_id                    = "${var.vpc_id}"
